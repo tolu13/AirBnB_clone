f44550f150a9d0b9c33bebad479522ac74e7cce1
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-12-08 19:37:01 +0000

    Airbnb clone

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This initialisez and creates a unique package"""
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
 	"""This is the parent class in which other class inherits from"""
@@ -11,6 +12,7 @@ class BaseModel:
 	    self.id = str(uuid.uuid4())
 	    self.created_at = datetime.now()
 	    self.updated_at = datetime.now()
+	    storage.new(self)
 
 	def __str__(self):
 	    """this returns official string representation"""
@@ -19,6 +21,7 @@ class BaseModel:
 	def save(self):
 	    """update the public instance attributes"""
 	    self.updated_at = datetime.now()
+	    storage.save()
 
 	def to_dict(self):
 	    """this returns dictionary containing key/value"""
