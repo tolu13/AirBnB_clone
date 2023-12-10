41dd7a650412676a3c92ffb612aff1d07b75be75
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-12-10 08:41:19 +0000

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,7 @@ class FileStorage:
 	__objects = {}
 
 	def all(self):
+	
 		"""This returns the dictionary objects"""
 		return FileStorage.__objects
 
@@ -51,4 +52,42 @@ class FileStorage:
                     obj_dict = {key: self.classes()[value["__class__"]](**value)
                                 for key, value in obj_dict.items()}
                     # TODO: should this overwrite or insert?
-                    FileStorage.__objects = obj_dict#!
+                    FileStorage.__objects = obj_dict
+      
+	def attributes(self):
+		"""This returns the valid attributes and their types and classname"""
+		attributes = {
+		"BaseModel":
+			{"id": str,
+			"created_at": datetime.datetime,
+			"updated_at": datetime.datetime},
+		"User":
+			{"email": str,
+			"password": str,
+			"first_name": str,
+			"last_name": str},
+		"State":
+			{"name": str},
+		"City":
+			{"state_id": str,
+			"name": str},
+		"Amenity":
+			{"name": str},
+		"Place":
+			{"city_id": str,
+			"user_id": str,
+			"name": str,
+			"description": str,
+			"number_rooms": int,
+			"number_bathrooms": int,
+			"max_guest": int,
+			"price_by_night": int,
+			"latitude": float,
+			"longitude": float,
+			"amenity_ids": list},
+		"Review":
+			{"place_id": str,
+			"user_id": str,
+			"text": str}
+		}
+		return attributes
